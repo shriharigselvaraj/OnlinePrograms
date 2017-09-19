cd92c94cc3708e4635cfcc339cc102bc5d6c0755
shriharigselvaraj <shrihari.gs@gmail.com>
2017-09-19 22:12:08 +0530

    Completed first program

--- practicepython/characterInput.py
+++ practicepython/characterInput.py
@@ -1,4 +1,12 @@
 #Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the
 # year that they will turn 100 years old.
+from builtins import input
 
-print("Enter the number")
+name=input("Enter your name\n")
+age=int(input("Enter your age"))
+repeat=int(input("Enter the number of time the message should be displayed"))
+
+if(age<101):
+    print(repeat*(name+", You are going to turn 100 in "+str(100-age)+"\n"))
+else:
+    print(repeat*("You are already crossed 100 years, All the best \n"))
